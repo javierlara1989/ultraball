cd8502c23a9322fd37794345c4666f27f4f0a750
Javier <javier.lara@usach.cl>
2016-12-07 14:34:29 -0300

    gh-pages - Sky colors

--- js/ultraball.js
+++ js/ultraball.js
@@ -4,11 +4,23 @@ clouds = ["/img/clouds/clouds1.png",
           "/img/clouds/clouds2.png",
           "/img/clouds/clouds3.png"];
 
+skyColours = ["#001848", "#001848", "#001848", "#301860", "#301860", "#483078",
+              "#604878", "#906090", "#0CC3FA", "#61D6FA", "#89E0FA" ,"#89E0FA",
+              "#B4EAFA", "#B4EAFA", "#B4EAFA", "#B4EAFA", "#7DAECC" ,"#4B93BF",
+              "#368ABE", "#167AB8", "#483078", "#301860", "#001848", "#001848"];
+
 (function() {
   $(document).ready(onReady);
 
   function onReady() {
+    setWeather();
+  };
+
+  function setWeather(){
     var cloudDiv = document.getElementById("cloud");
     cloudDiv.src = clouds[Math.floor(Math.random()*clouds.length)];
-  };
+
+    var currentTime = new Date().getHours();
+    document.body.style.backgroundColor = skyColours[currentTime];
+  }
 })();
