634ff90fce0cd70e3232e16730083777aaec5279
Javier <javier.lara@usach.cl>
2016-12-09 15:25:40 -0300

    gh-pages - js > jquery

--- js/ultraball.js
+++ js/ultraball.js
@@ -19,8 +19,8 @@ skyColours = ["#001848", "#001848", "#001848", "#301860", "#301860", "#483078",
   function setWeather(){
     var cloudDiv = document.getElementById("cloud");
     cloudDiv.src = clouds[Math.floor(Math.random()*clouds.length)];
-    cloudDiv.css("width", "100%");
-    cloudDiv.css("height", "100%");
+    cloudDiv.style.width = "100%";
+    cloudDiv.style.height = "100%";
 
     var currentTime = new Date().getHours();
     document.body.style.backgroundColor = skyColours[currentTime];
