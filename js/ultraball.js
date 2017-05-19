385d9f68859542ce26862e94b8947e2a1bd0d1ba
Javier Lara <javier@admetricks.com>
2017-05-19 17:53:29 -0400

    [gh-pages] Added boostrap start theme

--- js/ultraball.js
+++ js/ultraball.js
@@ -1,8 +1,8 @@
 /* global js*/
 
-clouds = ["/img/clouds/clouds1_low.png",
-          "/img/clouds/clouds2_low.png",
-          "/img/clouds/clouds3_low.png"];
+clouds = ["img/clouds/clouds1_low.png",
+          "img/clouds/clouds2_low.png",
+          "img/clouds/clouds3_low.png"];
 
 skyColours = ["#001848", "#001848", "#001848", "#301860", "#301860", "#483078",
               "#604878", "#906090", "#0CC3FA", "#61D6FA", "#89E0FA" ,"#89E0FA",
@@ -20,6 +20,10 @@ skyColours = ["#001848", "#001848", "#001848", "#301860", "#301860", "#483078",
     var cloudDiv = document.getElementById("cloud");
     cloudDiv.src = clouds[Math.floor(Math.random()*clouds.length)];
     cloudDiv.style.width = "100%";
+
+    var starsDiv = document.getElementById("stars");
+    starsDiv.src = "img/stars/stars_01.gif";
+    starsDiv.style.width = "100%";
 
     var currentTime = new Date().getHours();
     document.body.style.backgroundColor = skyColours[currentTime];
