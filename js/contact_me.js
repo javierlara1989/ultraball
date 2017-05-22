eae0b8f591db5e0b34b5b58ab1ddc5c04651cf0a
Javier Lara <javier@admetricks.com>
2017-05-22 15:31:21 -0400

    [gh-pages] formspree test

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/javier.lara@usach.cl",
                 type: "POST",
                 data: {
                     name: name,
